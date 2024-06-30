42306eed98055c223d533345d4880b0ce850851c
Sachin Mishra <96466673+sachin2709@users.noreply.github.com>
2024-06-30 07:42:48 +0530

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://127.0.0.1:5173',
   'http://localhost:3000',
-  'http://192.168.43.93:3000'
+  'http://192.168.43.93:3000',
+  'https://revolution2o.netlify.app/'
 ];
 
 const corsOptions = {
